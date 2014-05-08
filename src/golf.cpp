012169c9ff820e8b724ac29b7c76d69241ee7d8d
Richard Freling <rfreling@princeton.edu>
2014-05-08 16:04:55 -0400

    Change dependencies in fglut

--- src/golf.cpp
+++ src/golf.cpp
@@ -15,25 +15,25 @@ float deltaMove = 0;
 int xOrigin = -1;
 
 void changeSize(int w, int h) {
-    
+
 	// Prevent a divide by zero, when window is too short
 	// (you cant make a window of zero width).
 	if (h == 0)
 		h = 1;
 	float ratio =  w * 1.0 / h;
-    
+
 	// Use the Projection Matrix
 	glMatrixMode(GL_PROJECTION);
-    
+
 	// Reset Matrix
 	glLoadIdentity();
-    
+
 	// Set the viewport to be the entire window
 	glViewport(0, 0, w, h);
-    
+
 	// Set the correct perspective.
 	gluPerspective(45.0f, ratio, 0.1f, 100.0f);
-    
+
 	// Get Back to the Modelview
 	glMatrixMode(GL_MODELVIEW);
 }
@@ -45,37 +45,37 @@ void drawBox() {
 }
 
 void computePos(float deltaMove) {
-    
+
 	x += deltaMove * lx * 0.1f;
 	z += deltaMove * lz * 0.1f;
 }
 
 void computeDir(float deltaAngle) {
-    
+
 	angle += deltaAngle;
 	lx = sin(angle);
 	lz = -cos(angle);
 }
 
 void renderScene(void) {
-    
+
 	if (deltaMove)
 		computePos(deltaMove);
 	if (deltaAngle)
 		computeDir(deltaAngle);
-    
+
 	// Clear Color and Depth Buffers
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+
 	// Reset transformations
 	glLoadIdentity();
 	// Set the camera
 	gluLookAt(	x, 1.0f, z,
               x+lx, 1.0f,  z+lz,
               0.0f, 1.0f,  0.0f);
-    
+
     // Draw ground
-    
+
 	glColor3f(0.9f, 0.9f, 0.9f);
 	glBegin(GL_QUADS);
     glVertex3f(-100.0f, 0.0f, -100.0f);
@@ -83,16 +83,16 @@ void renderScene(void) {
     glVertex3f( 100.0f, 0.0f,  100.0f);
     glVertex3f( 100.0f, 0.0f, -100.0f);
 	glEnd();
-    
+
     // Draw Golf Ball
-    
+
     glColor3f(0.0f, 0.2f, 1.0f);
     glTranslatef(3.0f, 0.25f, 2.0f);
     glutSolidSphere(0.25f, 20, 20);
     glTranslatef(-3.0f, -0.25f, -2.0f);
-    
+
     // Draw 36 Boxes
-    
+
 	for(int i = -3; i < 3; i++)
 		for(int j=-3; j < 3; j++) {
 			glPushMatrix();
@@ -100,18 +100,18 @@ void renderScene(void) {
 			drawBox();
 			glPopMatrix();
 		}
-    
+
 	glutSwapBuffers();
 }
 
 void processNormalKeys(unsigned char key, int xx, int yy) {
-    
+
     if (key == 27)
         exit(0);
 }
 
 void pressKey(int key, int xx, int yy) {
-    
+
 	switch (key) {
 		case GLUT_KEY_LEFT : deltaAngle = -0.01f; break;
 		case GLUT_KEY_RIGHT : deltaAngle = 0.01f; break;
@@ -121,7 +121,7 @@ void pressKey(int key, int xx, int yy) {
 }
 
 void releaseKey(int key, int x, int y) {
-    
+
 	switch (key) {
 		case GLUT_KEY_LEFT :
 		case GLUT_KEY_RIGHT : deltaAngle = 0.0f;break;
@@ -131,13 +131,13 @@ void releaseKey(int key, int x, int y) {
 }
 
 void mouseMove(int x, int y) {
-    
+
     // this will only be true when the left button is down
     if (xOrigin >= 0) {
-        
+
 		// update deltaAngle
 		deltaAngle = (x - xOrigin) * 0.001f;
-        
+
 		// update camera's direction
 		lx = sin(angle + deltaAngle);
 		lz = -cos(angle + deltaAngle);
@@ -145,10 +145,10 @@ void mouseMove(int x, int y) {
 }
 
 void mouseButton(int button, int state, int x, int y) {
-    
+
 	// only start motion if the left button is pressed
 	if (button == GLUT_LEFT_BUTTON) {
-        
+
 		// when the button is released
 		if (state == GLUT_UP) {
 			angle += deltaAngle;
@@ -168,18 +168,18 @@ void GLUTInit(int *argc, char **argv)
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(640,640);
 	glutCreateWindow("Cube Field");
-    
+
 	// register callbacks
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
 	glutIdleFunc(renderScene);
-    
+
 	glutSpecialFunc(pressKey);
 	glutIgnoreKeyRepeat(1);
 	glutSpecialUpFunc(releaseKey);
 	glutMouseFunc(mouseButton);
 	glutMotionFunc(mouseMove);
-    
+
 	// OpenGL init
 	glEnable(GL_DEPTH_TEST);
 }
@@ -188,9 +188,9 @@ void GLUTInit(int *argc, char **argv)
 int main(int argc, char **argv) {
 	// init GLUT and create window
 	GLUTInit(&argc, argv);
-    
+
     // enter GLUT event processing cycle
 	glutMainLoop();
-    
+
 	return 1;
 }
